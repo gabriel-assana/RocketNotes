b6d20ffd782ca8ae4c28a0d110fc49df5e8ae610
Gabriel Assana <gabrielhhz2@gmail.com>
2022-09-26 21:34:30 -0300

    Validando usuário cadastrado

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,10 +1,18 @@
+const knex = require('../database/knex');
+const AppError = require('../utils/AppError')
+
 class SessionsController {
 
     async create(request, response){
         const { email, password } = request.body
 
+        const user = await knex("users").where({ email }).first();
 
-        return response.json({ email, password })
+        if(!user) {
+            throw new AppError("Email e/ou senha incorreta", 401);
+        }
+
+        return response.json(user);
     }
 }
 
