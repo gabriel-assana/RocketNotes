f4653922d4ef6f567ead4c16356128d4b06c367f
Gabriel Assana <gabrielhhz2@gmail.com>
2022-10-22 16:48:11 -0300

    resolvendo filtro de tags duplicadas

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,8 +1,8 @@
 const knex = require("../database/knex");
 const AppError = require("../utils/AppError");
-const {compare} = require("bcryptjs");
+const { compare } = require("bcryptjs");
 const authConfig = require("../configs/auth");
-const {sign} = require("jsonwebtoken");
+const { sign } = require("jsonwebtoken");
 
 
 class SessionsController {
@@ -12,13 +12,13 @@ class SessionsController {
     const user = await knex("users").where({email}).first();
 
     if(!user) {
-      throw new AppError("E-mail e ou senha incorreta", 401);
+      throw new AppError("E-mail e/ou senha incorreta", 401);
     }
 
     const passwordMatched = await compare(password, user.password);
 
     if(!passwordMatched) {
-      throw new AppError("E-mail e ou senha incorreta", 401);
+      throw new AppError("E-mail e/ou senha incorreta", 401);
     }
 
     const { secret, expiresIn } = authConfig.jwt;
